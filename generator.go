0674822a31aaddd79dddfbf314d699b07ec4d278
antonkholodok <akholodok@gmail.com>
2018-06-22 14:19:57 +0300

    waiting generator

--- generator.go
+++ generator.go
@@ -6,12 +6,30 @@ import (
 	"time"
 )
 
+type WaitingMessage struct {
+	msg  string
+	wait chan bool
+}
+
 func generator(msg string) <-chan string {
 	c := make(chan string)
 	go func() {
 		for i := 0; ; i++ {
 			c <- fmt.Sprintf("%s %d", msg, i)
 			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+	return c
+}
+
+func waitingGenerator(msg string) <-chan WaitingMessage {
+	c := make(chan WaitingMessage)
+	go func() {
+		waitForIt := make(chan bool)
+		for i := 0; ; i++ {
+			c <- WaitingMessage{fmt.Sprintf("%s %d", msg, i), waitForIt}
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+			<-waitForIt
 		}
 	}()
 	return c
