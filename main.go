6c228353445f85bc5ce2d92bda9d5192f49b9480
antonkholodok <akholodok@gmail.com>
2018-06-22 13:39:03 +0300

    generator

--- main.go
+++ main.go
@@ -3,10 +3,22 @@ package main
 import "fmt"
 
 func main() {
+	generatorExec()
+}
+
+func boringExec() {
 	c := make(chan string)
 	go boring("message", c)
 	for i := 0; i < 5; i++ {
 		fmt.Printf("received message: %q\n", <-c)
 	}
 	fmt.Println("completed")
+}
+
+func generatorExec() {
+	c := generator("generator")
+	for i := 0; i < 5; i++ {
+		fmt.Printf("Generator said: %q\n", <-c)
+	}
+	fmt.Println("Leaving...")
 }
